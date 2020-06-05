051858a508a95b7dfd3d55d9de8de7a81ecb486f
toon <nu.chai56@gmail.com>
2020-06-05 09:30:40 -0500

    set model interacting to db

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Api runnig'));
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/foods", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log('DB Connected'));
 
 const PORT = process.env.PORT || 1000;
 
